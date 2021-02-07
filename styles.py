66a409028bfa6ea147e2fa6b025e3e5c159df0a8
librarian-hive <67760249+librarian-hive@users.noreply.github.com>
2021-02-06 21:04:42 -0800

    Update styles.py

--- styles.py
+++ styles.py
@@ -1,51 +1,95 @@
 style = """
 <style> 
-    .e1fqkh3o2 svg {
-        color: black;
-    }
-    .main {
-        background-image: linear-gradient(rgb(33,36,41),rgb(225,26,60));
-    }
-    img {
-        border-radius: 10px;
-    }
-    svg {
-        color: white;
-    }
-    .css-xq1lnh-EmotionIconBase {
-        color: white;
-    }
-    .comment {
-        color:white;
-        font-size:15px;
-        border: thin solid black;
-        padding: 10px;
-        margin: 10px;
-        border-radius: 10px;
-        background-color: rgb(33,36,41);
-    }
-    .body_string {
-        color: lightgray;
-    }
-    .quote {
-        color: gray;
-    }
-    .css-1aumxhk { 
-        background-image: linear-gradient(rgb(225,26,60), rgb(33,36,41));
-        color: gray; 
-    } 
-    .st-bw,  .st-bv {
-        background-color: rgb(256, 254, 192);
-        color: black;
-    }
-    .css-145kmo2 {
-        font-size: 1rem;
-        color: rgb(256, 256, 256);
-        margin-bottom: 0.4rem;
-    }
-    .css-2trqyj {
-        background-color: gray;
-        color: black;
-    }
+p > img {
+    width: 100%;
+    height: auto;
+}
+
+.main {
+background: rgb(227,237,247);
+background: linear-gradient(346deg, rgba(227,237,247,1) 1%, rgba(154,152,152,1) 100%);
+}
+
+.main .e16nr0p30 {
+    border: thin gray solid;
+    padding: 10px;
+    margin: 5px;
+    background-color: #fff;
+    box-shadow: 6px 6px 10px -1px rgba(0,0,0,0.15),
+                -6px -6px 10px -1px rgba(255,255,255,0.7);
+    border: 1px solid rgba(0,0,0,0);
+    background-color: #e3edf7;
+
+}
+.comment {
+    text-align: left;
+    background-color: rgb(224,26,60);
+    clip-path: polygon(0 0, 75% 0, 63% 100%, 0 100%);
+    padding: 10px;
+    margin: -10px -10px 10px -10px;
+    color: #fff;
+}
+
+.comment a {
+    color: #fff;
+}
+
+.total_comments {
+    text-align: center;
+    font-size: 20px;
+    color: #f00;
+}
+
+.css-1aumxhk { 
+    /*background: rgb(225,225,234);*/
+    background: #e3edf7;
+} 
+
+.css-1aumxhk hr {
+    border-top: 1px solid red;
+}
+
+.css-1aumxhk .edgvbvh1 {
+    display: flex;
+    background: #e3edf7;
+    height: 75px;
+    width: 200px;
+    
+    border-radius: 8px;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    box-shadow: 6px 6px 10px -1px rgba(0,0,0,0.15),
+                -6px -6px 10px -1px rgba(255,255,255,0.7);
+    border: 1px solid rgba(0,0,0,0);
+    transition: transform 0.5s;
+    font-size: 30px;
+    color: #777;
+    width: 300px;
+}
+
+.css-1aumxhk .edgvbvh1:hover  {
+    box-shadow: inset 4px 4px 6px -1px rgba(0,0,0,0.2),
+                inset -4px -4px 6px -1px rgba(255,255,255,0.7),
+                -0.5px -0.5px 0px rgba(255,255,255,1),
+                0.5px 0.5px 0px rgba(0,0,0,0.15),
+                0px 12px 10px -10px rgba(0,0,0,0.05);
+    border: 1px solid rgba(0,0,0,0.01);
+    transform: translateY(2px);
+    transform: scale(0.90);
+    color: #f00;
+
+}
+
+.user_profile {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+}
+
+.main h1 {
+    text-align: center;
+}
+
 </style>
 """
